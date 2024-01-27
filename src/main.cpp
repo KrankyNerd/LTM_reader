197713dac7d4a3ab7340eecac5775fd5349b2134
KrankyNerd <153078271+KrankyNerd@users.noreply.github.com>
2024-01-27 20:18:46 +0100

    added voltage, battery consumption, rssi fixed sh*t idk man

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,21 @@
-//FIXME:MAKE SURE SWITCH SB IS DOWN OTHERWISE NO DATA!!!!
-
+// FIXME:MAKE SURE SWITCH SB IS DOWN OTHERWISE NO DATA!!!!
+//use read int to read uint16
 #include <Arduino.h>
 
 /* #################################################################################################################
  * LightTelemetry protocol (LTM)
  *
- * Ghettostation one way telemetry protocol for really low bitrates (1200/2400 bauds). 
- *			   
+ * Ghettostation one way telemetry protocol for really low bitrates (1200/2400 bauds).
+ *
  * Protocol details: 3 different frames, little endian.
  *   G Frame (GPS position) (2hz @ 1200 bauds , 5hz >= 2400 bauds): 18BYTES
- *    0x24 0x54 0x47 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF  0xFF   0xC0   
+ *    0x24 0x54 0x47 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF  0xFF   0xC0
  *     $     T    G  --------LAT-------- -------LON---------  SPD --------ALT-------- SAT/FIX  CRC
  *   A Frame (Attitude) (5hz @ 1200bauds , 10hz >= 2400bauds): 10BYTES
- *     0x24 0x54 0x41 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xC0   
+ *     0x24 0x54 0x41 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF 0xC0
  *      $     T   A   --PITCH-- --ROLL--- -HEADING-  CRC
  *   S Frame (Sensors) (2hz @ 1200bauds, 5hz >= 2400bauds): 11BYTES
- *     0x24 0x54 0x53 0xFF 0xFF  0xFF 0xFF    0xFF    0xFF      0xFF       0xC0     
+ *     0x24 0x54 0x53 0xFF 0xFF  0xFF 0xFF    0xFF    0xFF      0xFF       0xC0
  *      $     T   S   VBAT(mv)  Current(ma)   RSSI  AIRSPEED  ARM/FS/FMOD   CRC
  * ################################################################################################################# */
 /*
@@ -34,45 +34,46 @@ enum LTMStates
 
 const int LONGEST_FRAME_LENGTH = 18;
 
-//define the length of each frame type
+// define the length of each frame type
 const int G_FRAME_LENGTH = 18;
-const int A_FRAME_LENGTH = 10; //1 byte function, 6 byte payload, 3 byte CRC
+const int A_FRAME_LENGTH = 10; // 1 byte function, 6 byte payload, 3 byte CRC
 const int S_FRAME_LENGTH = 11;
 const int O_FRAME_LENGTH = 18;
 const int N_FRAME_LENGTH = 10;
 const int X_FRAME_LENGTH = 10;
 
-//define the data type of each variable, this should match up with the LTM docs
+// define the data type of each variable, this should match up with the LTM docs
 typedef struct RemoteData_s
 {
-  //A frame (attitude) 6 bytes total
+  // A frame (attitude) 6 bytes total
   int16_t pitch;
   int16_t roll;
   int16_t heading;
 
-  //S frame (status) 7 bytes
-  uint16_t voltage;   //2 bytes
-  byte rssi;          //1 byte
+  // S frame (status) 7 bytes
+  uint16_t voltage; // 2 bytes
+  uint16_t batteryConsumption; //2 bytes
+  byte rssi;        // 1 byte
   bool armed;
   bool failsafe;
   byte flightmode;
-  
-  //G frame (GPS) 14 bytes //something in here is wrong me thinks
-  int32_t latitude;   //4 bytes
-  int32_t longitude;  //4 bytes
-  int32_t altitude;   //4 bytes
-  uint8_t groundSpeed;//1 byte
 
-  int16_t hdop; //part of X frame
+  // G frame (GPS) 14 bytes //something in here is wrong me thinks
+  int32_t latitude;    // 4 bytes
+  int32_t longitude;   // 4 bytes
+  int32_t altitude;    // 4 bytes
+  uint8_t groundSpeed; // 1 byte
+
+  int16_t hdop; // part of X frame
   uint8_t gpsFix;
   uint8_t gpsSats;
 
-  //O frame (origin) 14 bytes
-  int32_t homeLatitude;   //4 bytes
-  int32_t homeLongitude;  //4 bytes
-  uint32_t homeAltitude;  //4 bytes
-  uint8_t OSD;            //1 byte //these are the same as uchar
-  uint8_t homeFix;        //1 byte 
+  // O frame (origin) 14 bytes
+  int32_t homeLatitude;  // 4 bytes
+  int32_t homeLongitude; // 4 bytes
+  uint32_t homeAltitude; // 4 bytes
+  uint8_t OSD;           // 1 byte //these are the same as uchar
+  uint8_t homeFix;       // 1 byte
 
   uint8_t sensorStatus;
 } RemoteData_t;
@@ -112,23 +113,24 @@ int32_t readInt32(uint8_t offset)
 
 void parseFrame()
 {
-  if (frameType == 'A') //attitude
+  if (frameType == 'A') // attitude //FIXME:
   {
     remoteData.pitch = readInt16(0);
     remoteData.roll = readInt16(2);
     remoteData.heading = readInt16(4);
   }
 
-  if (frameType == 'S') //status
+  if (frameType == 'S') // status
   {
-    remoteData.voltage = readInt(0);
+    remoteData.voltage = readInt(0); //this part works
+    remoteData.batteryConsumption = readInt(2); //this works
     remoteData.rssi = readByte(4);
 
     byte raw = readByte(6);
     remoteData.flightmode = raw >> 2;
   }
 
-  if (frameType == 'G') //gps this works
+  if (frameType == 'G') // gps this works
   {
     remoteData.latitude = readInt32(0);
     remoteData.longitude = readInt32(4);
@@ -142,13 +144,15 @@ void parseFrame()
 
   if (frameType == 'X')
   {
-    remoteData.hdop = readInt(0);//FIXME: I think this should be uint16
+    remoteData.hdop = readInt(0); //its a uint therefore read int rather than int16
     remoteData.sensorStatus = readByte(2);
   }
 
-  if(frameType == 'O');
+  if (frameType == 'O')
+    ;
 
-  if(frameType == 'N');
+  if (frameType == 'N')
+    ;
 }
 
 void readData()
@@ -156,7 +160,7 @@ void readData()
   if (Serial1.available())
   {
     char data = Serial1.read();
-    //Serial.println(data);
+    // Serial.println(data);
 
     if (state == IDLE)
     {
@@ -182,7 +186,7 @@ void readData()
       state = HEADER_MSGTYPE;
       receiverIndex = 0;
 
-      switch (data)//based on the data in the header_start 2, figure out the length of the frame
+      switch (data) // based on the data in the header_start 2, figure out the length of the frame
       {
       case 'G':
         frameLength = G_FRAME_LENGTH;
@@ -207,78 +211,76 @@ void readData()
       }
     }
     else if (state == HEADER_MSGTYPE)
-{
-  if (receiverIndex == frameLength - 4)
-  {
-    parseFrame();
-    state = IDLE;
-    memset(serialBuffer, 0, LONGEST_FRAME_LENGTH);
-  }
-  else if (receiverIndex >= LONGEST_FRAME_LENGTH)
-  {
-    // Buffer overflow, reset state to IDLE
-    state = IDLE;
-  }
-  else
-  {
-    serialBuffer[receiverIndex++] = data;
-  }
-}
-
+    {
+      if (receiverIndex == frameLength - 4)
+      {
+        parseFrame();
+        state = IDLE;
+        memset(serialBuffer, 0, LONGEST_FRAME_LENGTH);
+      }
+      else if (receiverIndex >= LONGEST_FRAME_LENGTH)
+      {
+        // Buffer overflow, reset state to IDLE
+        state = IDLE;
+      }
+      else
+      {
+        serialBuffer[receiverIndex++] = data;
+      }
+    }
   }
 }
 
 void serialPrintData()
 {
-    // Serial prints
-    Serial.print("Lat:");
-    Serial.println(remoteData.latitude);
+  // Serial prints
+  Serial.print("Lat:");
+  Serial.println(remoteData.latitude);
 
-    Serial.print("Lon:");
-    Serial.println(remoteData.longitude);
+  Serial.print("Lon:");
+  Serial.println(remoteData.longitude);
 
-    Serial.print("HDOP:");
-    Serial.println(remoteData.hdop);
+  Serial.print("HDOP:");
+  Serial.println(remoteData.hdop);
 
-    // Serial.print("Fix:");
-    // Serial.println(fixTypes[remoteData.gpsFix]);
+  Serial.print("Fix:");
+  Serial.println(fixTypes[remoteData.gpsFix]);
 
-    Serial.print("Fix:");
-    if (remoteData.gpsFix >= 0 && remoteData.gpsFix < sizeof(fixTypes) / sizeof(fixTypes[0]))
-    {
-      Serial.println(fixTypes[remoteData.gpsFix]);
-    }
-    else
-    {
-      Serial.println("Unknown");
-    }
+  Serial.print("Sat:");
+  Serial.println(remoteData.gpsSats);
 
-    Serial.print("Sat:");
-    Serial.println(remoteData.gpsSats);
+  Serial.print("Spd:");
+  Serial.println(remoteData.groundSpeed);
 
-    Serial.print("Spd:");
-    Serial.println(remoteData.groundSpeed);
+  Serial.print("Alt:");
+  Serial.println(remoteData.altitude);
 
-    Serial.print("Alt:");
-    Serial.println(remoteData.altitude);
+  Serial.print("Roll:");
+  Serial.println(remoteData.roll);
 
-    Serial.print("Roll:");
-    Serial.println(remoteData.roll);
+  Serial.print("Pitch:");
+  Serial.println(remoteData.pitch);
 
-    Serial.print("Pitch:");
-    Serial.println(remoteData.pitch);
+  Serial.print("Voltage:");
+  Serial.println(remoteData.voltage/1000.);
+
+  Serial.print("Battery Consumption:");
+  Serial.println(remoteData.batteryConsumption/1000.);
+
+  Serial.print("RSSI:"); //is correct
+  Serial.println(remoteData.rssi);
 }
 
 void setup()
 {
-  Serial.begin(2400); //begin serial with pc
-  Serial1.begin(115200); //begin serial with tx16s
+  Serial.begin(2400);    // begin serial with pc
+  Serial1.begin(115200); // begin serial with tx16s
 }
 
 unsigned long nextDisplay = 0;
 
 void loop()
-{ 
+{
   readData();
   if (millis() >= nextDisplay)
   {
