774b064252f741dd7da9ca894e76f1410ab720c9
KrankyNerd <153078271+KrankyNerd@users.noreply.github.com>
2024-01-29 16:37:51 +0100

    gps, voltage, mah, works, DISPLAY WORKS!!!!

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,26 @@
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+static const unsigned char PROGMEM logo_bmp[] =
+{ 0b00000000, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000011, 0b11100000,
+  0b11110011, 0b11100000,
+  0b11111110, 0b11111000,
+  0b01111110, 0b11111111,
+  0b00110011, 0b10011111,
+  0b00011111, 0b11111100,
+  0b00001101, 0b01110000,
+  0b00011011, 0b10100000,
+  0b00111111, 0b11100000,
+  0b00111111, 0b11110000,
+  0b01111100, 0b11110000,
+  0b01110000, 0b01110000,
+  0b00000000, 0b00110000 };
+
 
 /* #################################################################################################################
  * LightTelemetry protocol (LTM)
@@ -292,8 +312,21 @@ void displayTelem()
   display.setTextColor(SSD1306_WHITE); // Draw white text
   display.setCursor(0, 0);     // Start at top-left corner
   //display.cp437(true);         // Use full 256 char 'Code Page 437' font
-  display.println(F("Voltage:"));
+  
+  //display GPS
+  display.print(F("Lat: "));
+  display.println(remoteData.latitude);
+  display.print(F("Long: "));
+  display.println(remoteData.longitude);
+  
+  //display Battery info
+  display.print(F("Voltage:"));
   display.print(remoteData.voltage/1000.);
+  display.println(F("V"));
+
+  display.print(F("Consumed: "));
+  display.print(remoteData.batteryConsumption);
+  display.println(F("mah"));
 
   display.display();
 }
@@ -304,22 +337,26 @@ void setup()
   Serial1.begin(115200); // begin serial with tx16s
   //while(!Serial);
 
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
   // Show initial display buffer contents on the screen --
   // the library initializes this with an Adafruit splash screen.
-  // display.display();
-  // delay(2000); // Pause for 2 seconds
+  display.display();
+  delay(2000); // Pause for 2 seconds
 
-  // // Clear the buffer
-  // display.clearDisplay();
+  // Clear the buffer
+  display.clearDisplay();
 
-  // // Draw a single pixel in white
-  // display.drawPixel(10, 10, SSD1306_WHITE);
-  // Serial.println("Init display");
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_WHITE);
+  Serial.println("Init display");
 
-  // // Show the display buffer on the screen. You MUST call display() after
-  // // drawing commands to make them visible on screen!
-  // display.display();
-  // delay(2000);
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
 }
 
 unsigned long nextDisplay = 0;
